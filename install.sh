45763053210c34c797392922d13b76d05fa34429
Enzo Bronzon <108149923+EnzoGly@users.noreply.github.com>
2022-09-11 20:22:20 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ bash start-kali.sh
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xvzf ngrok-v3-stable-linux-amd64.tgz
 rm ngrok-v3-stable-linux-amd64.tgz
+exit
 
 
 
