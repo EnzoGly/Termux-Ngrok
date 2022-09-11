d9d2d5d6962b90f09810f6d1e156de897d90bcac
Enzo Bronzon <108149923+EnzoGly@users.noreply.github.com>
2022-09-11 20:31:58 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,8 @@ pkg install wget
 wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh
 chmod +x * 
 bash kali.sh
-bash start-kali.sh
-wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-tar -xvzf ngrok-v3-stable-linux-amd64.tgz
-rm ngrok-v3-stable-linux-amd64.tgz
-exit
+
+
 
 
 
