9bdeac11a1d56670bebd816d8ff75133a4b025d0
Enzo Bronzon <108149923+EnzoGly@users.noreply.github.com>
2022-09-11 21:21:08 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,13 @@ pkg upgrade
 pkg install python3
 pkg install wget
 wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh
-git https://github.com/EnzoGly/API
+cp kali.sh $HOME
+git clone https://github.com/EnzoGly/API
 cd API
 cp programa.py $HOME
-cd 
+cd ..
 rm -rf API
+cd $HOME
 chmod +x * 
 bash kali.sh
 
